b92d29e27a809646283526409aa3ce4ddef1f993
deepthishetty <deepthish24@gmail.com>
2016-07-10 14:58:17 +0530

    Update Democ.java

--- src/qsp/Democ.java
+++ src/qsp/Democ.java
@@ -4,7 +4,7 @@ public class Democ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hello hello abc ");
+System.out.println("hello hello abc cde");
 	}
 
 }
